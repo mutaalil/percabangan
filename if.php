f9400b9826722a943d8f6c8f5ff3ad77212d2092
mutaalil <mutaalilmatin@gmail.com>
2023-11-12 21:46:29 +0700

    contohsoalphp1

---
+++ if.php
@@ -0,0 +1,12 @@
+<?php
+$umur = 15;
+
+if ($umur < 18 )
+{
+    echo " Anda tidak dapat membuka situs ini";
+}
+else 
+{
+    echo "Selamat datang di website kami";
+}
+?>
---
+++ switch.php
@@ -0,0 +1,23 @@
+<?php
+ 
+$budgetSaya = 3000000;
+ 
+switch ($budgetSaya) {
+ case 40000000:
+  echo 'Kamu bisa beli motor 250 cc.';
+  break;
+ 
+ case 25000000:
+  echo 'Kamu bisa beli motor 150 cc.';
+  break;
+ 
+ case 20000000:
+  echo 'Kamu bisa beli motor matic.';
+  break;
+  
+ default:
+  echo 'Kamu harus cari uang lagi yang banyak.';
+  break;
+}
+ 
+?>
